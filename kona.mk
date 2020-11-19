0d716e291bbf061905b7e0aa8d2076f101ff88eb
Chenyang Zhong <zhongcy95@gmail.com>
2020-11-18 21:06:47 -0500

    sm8250-common: build vendor variant of libchrome from source
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.secure_element@1.2-service": library "libchrome.so" not found: needed by /vendor/lib64/se_nq_extn_client.so in namespace (default)
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- kona.mk
+++ kona.mk
@@ -361,6 +361,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_$(TARGET_NFC_SKU)/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_$(TARGET_NFC_SKU)/com.nxp.mifare.xml
 
+# NQ Client
+PRODUCT_PACKAGES += \
+    libchrome.vendor
+
 # OMX
 PRODUCT_PACKAGES += \
     libOmxCore \
