51e836cb36898c2ed8c89eaca59d8c7cd316d872
kaffuchino233 <qkivan@proton.me>
2026-06-08 21:27:36 +0800

    sm8250-common: Move to HIDL NXP NFC
    * AIDL will work,but always crash.
    Have no idea about this, Rollback to HIDL now(at least it's worked).

--- kona.mk
+++ kona.mk
@@ -369,11 +369,11 @@ $(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.nxp \
-    com.android.nfc_extras \
     NfcNci \
-    SecureElement \
-    Tag
+    Tag \
+    android.hardware.nfc@1.2-service \
+    com.android.nfc_extras \
+    libnfc_vendor_extn_sys
 
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor
